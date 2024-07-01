8633767955098af9d231a8f63a5873db6fe95ab4
loki0107 <logeshbalajir@gmail.com>
2024-07-02 00:03:22 +0530

    Create Rock paper Scissors.py

---
+++ Rock paper Scissors.py
@@ -0,0 +1,22 @@
+import random
+options=["Rock","Paper","Scissors"]
+useroption=input("enter Rock or Paper or Scissors")
+computerchoice=random.choice(options)
+print("you chose the option ",useroption)
+print("computer chose the option",computerchoice)
+if useroption==computerchoice:
+    print("its a tie with the computer")
+elif useroption=="Rock" and computerchoice=="Scissors":
+    print("you win against the computer")
+elif useroption=="Paper" and computerchoice=="Rock":
+    print("you win against the computer")
+elif useroption=="Scissors" and computerchoice=="Paper":
+    print("you win against the computer")
+elif useroption=="Rock" and computerchoice=="Paper":
+    print("you lost against the computer")
+elif useroption=="Paper" and computerchoice=="Scissors":
+    print("you lost against the computer")
+elif useroption=="Scissors" and computerchoice=="Rock":
+    print("you lost against the computer")
+else:
+    print("invalid option choosen")
